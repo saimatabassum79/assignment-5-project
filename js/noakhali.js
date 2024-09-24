26dd7f701484d190af2eab5e7468fa45e0df3a7e
saimatabassum79 <saimatabassum@gmail.com>
2024-09-24 19:15:53 +0200

    added the noakhali section

---
+++ js/noakhali.js
@@ -0,0 +1,46 @@
+const foodAtNoakhaliSubmiteBtn = document.getElementById(
+    "foodAtNoakhaliSubmiteBtn"
+  );
+  
+  foodAtNoakhaliSubmiteBtn.addEventListener("click", (event) => {
+    event.preventDefault();
+    // flood at at Noakhali
+    const donationNoakhaliCampaniName =
+      document.getElementById("donation_name").innerText;
+    CommonFeatcher(
+      "doanteAmountOfNoakhali",
+      "floodAtNoakhaliDonateAmount",
+      donationNoakhaliCampaniName
+    );
+  });
+  
+  // donation btn decler
+  const foodAtFeniSubmiteBtn = document.getElementById("foodAtFeniSubmiteBtn");
+  
+  foodAtFeniSubmiteBtn.addEventListener("click", (event) => {
+    event.preventDefault();
+    // flood at at feni cmapain name
+    const donationFeniCampaniName =
+      document.getElementById("feni_campani_name").innerText;
+  
+    CommonFeatcher(
+      "doanteAmountOfFeni",
+      "floodAtFeniDonateAmount",
+      donationFeniCampaniName
+    );
+  });
+  
+  // donation btn decler
+  const AtQotaSubmiteBtn = document.getElementById("AtQotaSubmiteBtn");
+  
+  AtQotaSubmiteBtn.addEventListener("click", (event) => {
+    event.preventDefault();
+    // flood at AtQuota cmapnai
+    const donationQuotaCampaniName =
+      document.getElementById("quota_campani_name").innerText;
+    CommonFeatcher(
+      "doanteAmountOfquota",
+      "AtQuateDonateAmount",
+      donationQuotaCampaniName
+    );
+  });
