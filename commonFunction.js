041e83926ea26d22f9981c761fadedc2114aa70c
saimatabassum79 <saimatabassum@gmail.com>
2024-09-24 19:22:29 +0200

    added the commonFunction js

---
+++ commonFunction.js
@@ -0,0 +1,53 @@
+// total donated amount need (navbar amount)
+const totalDonationNeed = document.getElementById("totalDonationNeed");
+// donation history sava data
+const donationDataHistory = document.getElementById("donationDataHistory");
+// modal dialog pop up delcler
+const modal = document.getElementById("modal_dialog_box");
+
+const CommonFeatcher = (doanteAmount, id, title) => {
+  // Get the input field element
+  const inputElement = document.getElementById(doanteAmount);
+
+  // Get the value from the input field
+  const inputFiled = parseFloat(inputElement.value);
+
+  // card amount taka
+  const floodDonateAmount = parseFloat(document.getElementById(id).innerText);
+
+  // validation data
+  let amoutnValidationMessage = document.querySelector(".warning_message");
+  if (inputFiled < 0 || isNaN(inputFiled)) {
+    amoutnValidationMessage.innerText = "invaid amount";
+    return;
+  }
+
+  // card taka + input filed data increment
+  const totalDonateAmount = inputFiled + floodDonateAmount;
+  document.getElementById(id).innerText = totalDonateAmount;
+
+  // total donation amount need decrement
+  totalDonationNeed.innerText =
+    parseFloat(totalDonationNeed.innerText) - inputFiled;
+
+  // donattion amount add success blanK the input filed
+  document.getElementById(doanteAmount).value = "";
+  amoutnValidationMessage.innerText = "";
+  // show modal
+  modal.showModal();
+
+  const div = document.createElement("div");
+  div.className =
+    "bg-white p-[25px] rounded-2xl border border-[rgba(17,17,17,0.1)] border-solid mb-2";
+  div.innerHTML = `
+              <h4 class="text-xl font-bold text-[rgb(17,17,17)]">
+                ${inputFiled} Taka is ${title}
+              </h4>
+              <div class="text-[16px] font-light text-[rgba(17,17,17,0.7)] mt-4">
+                <span class="text-xl font-bold text-[rgb(17,17,17)]">
+                  Date :</span
+                >
+                ${new Date("2015-03-25")}
+                       </div>`;
+  donationDataHistory.appendChild(div);
+};
